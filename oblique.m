30e04d802f1b6db020de525fb8dd9c89e65af73c
tfmIV <tfm@princeton.edu>
2023-03-09 00:03:37 -0500

    Finished TW

--- oblique.m
+++ oblique.m
@@ -1,8 +1,8 @@
-function [pFactor, rhoFactor, Tfactor, M] = oblique(M_inf, t)
+function [pFactor, rhoFactor, Tfactor, M, beta] = oblique(M_inf, t)
     %Calculate conditions after a generic oblique shock
     gam = 1.4;
 
-    beta = beta_calc(M_inf,t,gam,0);
+    beta = beta_calc(M_inf,t*pi/180,gam,0)*180/pi;
 
     Mn = sqrt(((gam-1)*M_inf^2*sind(beta)^2+2)/(2*gam*M_inf^2*sind(beta)^2-(gam-1)));
     M = Mn / sind(beta - t);
