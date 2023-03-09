4fce0eb12177e82166ce0a22818db036fc8a2493
Yousuf Tariq-Shuaib <symit@princeton.edu>
2023-03-08 22:31:09 -0500

    dada

--- oblique.m
+++ oblique.m
@@ -1,11 +1,8 @@
 function [pFactor, rhoFactor, Tfactor, M] = oblique(M_inf, t)
     %Calculate conditions after a generic oblique shock
     gam = 1.4;
-    syms b;
-    
-    eqnBeta = tand(b-t)/tand(b) == (2+(gam-1)*M_inf^2*sind(b)^2)/((gam+1)*M_inf^2*sind(b)^2);
-    beta = vpasolve(eqnBeta, b, 20);
-    beta = double(beta);
+
+    beta = beta_calc(M_inf,t,gam,0);
 
     Mn = sqrt(((gam-1)*M_inf^2*sind(beta)^2+2)/(2*gam*M_inf^2*sind(beta)^2-(gam-1)));
     M = Mn / sind(beta - t);
--- shock_expansion_cp.m
+++ shock_expansion_cp.m
@@ -4,7 +4,22 @@ K_nose = M_inf * theta_nose;
 beta_nose = theta_nose * ((gamma+1)/4 + sqrt(((gamma+1)/4)^2 + 1 / K_nose^2);
 pnose_pinf = 1 + 2 * gamma / (gamma+1) * (((gamma + 1)/4 * K_nose + sqrt((gamma+1)/4)^2 * K_nose^2 + 1)^2 - 1);
 p_nose = pnose_pinf * p_inf;
+a_inf = sqrt(gamma * p_inf / rho_inf);
+v_inf = M_inf * a_inf;
 
 [areas, planform, centers, thetas] = calculate_areas(mesh);
+
+p_ratio = zeros(mesh.NumFaces, 1);
+rho_ratio = zeros(mesh.NumFaces, 1);
+T_ratio = zeros(mesh.NumFaces, 1);
+Machs = zeros(mesh.NumFaces, 1);
+pressures = zeros(mesh.NumFaces, 1);
+c_p = zeros(mesh.NumFaces, 1);
+
+for i=1:length(thetas)
+    [p_ratio(i), rho_ratio(i), T_ratio(i), Machs(i)] = expansion(M_nose, theta(i) - theta_nose);
+    pressures(i) = p_ratio * p_nose;
+    c_p(i) = (pressures(i) - p_inf) / (1 / 2 * rho_inf * v_inf^2);
+end
 
 end
