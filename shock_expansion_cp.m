775e2bab4faf5e8616a5dc5efc2865c3b554325f
Yousuf Tariq-Shuaib <symit@princeton.edu>
2023-03-09 00:48:02 -0500

    Probably working shock expansion

--- shock_expansion_cp.m
+++ shock_expansion_cp.m
@@ -1,12 +1,4 @@
-meshfile = 'fuselagemesh.stl';
-mesh = readSurfaceMesh(meshfile);
-M_inf = 7;
-p_inf = 80;
-rho_inf = 0.00104;
-theta_nose = 0.5;
-gamma = 1.4
-
-%function [c_p, pressures] = shock_expansion_cp(mesh, gamma, M_inf, p_inf, rho_inf, theta_nose)
+function [c_p, pressures] = shock_expansion_cp(mesh, gamma, M_inf, p_inf, rho_inf, theta_nose)
 
 K_nose = M_inf * theta_nose;
 beta_nose = theta_nose * ((gamma+1)/4 + sqrt(((gamma+1)/4)^2 + 1 / K_nose^2));
@@ -23,16 +15,21 @@ p_ratio = zeros(mesh.NumFaces, 1);
 pressures = zeros(mesh.NumFaces, 1);
 c_p = zeros(mesh.NumFaces, 1);
 
-length(thetas)
-
 for i=1:length(thetas)
-    p_ratio(i) = expansion(M_nose, thetas(i) - theta_nose);
+    p_ratio(i) = expansion(M_nose, theta_nose - thetas(i));
     pressures(i) = p_ratio(i) * p_nose;
     c_p(i) = (pressures(i) - p_inf) / (1 / 2 * rho_inf * v_inf^2);
-
-    if i > 18500
-        i
-    end
 end
 
-%end
+end
+
+
+figure(1)
+c = c_p;
+patch('Faces', mesh.Faces, 'Vertices', mesh.Vertices, 'CData', c, 'FaceColor', 'flat', 'EdgeColor', 'none')
+view(3)
+axis vis3d
+axis equal
+title('Fuselage Pressure Distribution')
+cbar = colorbar;
+cbar.Label.String = 'Pressure (Pa)';
